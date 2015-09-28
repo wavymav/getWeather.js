367405f12515189044d58804149a279584987942
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-28 17:31:23 -0400

    added error event handlers to the request object

--- weather.js
+++ weather.js
@@ -6,3 +6,9 @@ var http = require('http'),
 var req = http.get('http://api.openweathermap.org/data/2.5/weather?zip=20721,us=' + apiKey, function(res) {
 	console.log(res.statusCode);
 });
+
+// Handles any errors that may occur on the request object
+req.on('error', function(err) {
+	// Logs the error to the command line
+	console.log('Got an error: ' + err.message);
+});
