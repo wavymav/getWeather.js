68241ac62cadf8c7a4dfe8438ca9d1480ad3c97b
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-28 17:12:04 -0400

    set up the http .get() method to test API response status code

--- weather.js
+++ weather.js
@@ -1,3 +1,8 @@
 // Requiring one of Node's built in modules 'http'
 var http = require('http'),
 		apiKey = require('./apiKey');
+
+// Setting up the http .get() method
+var req = http.get('http://api.openweathermap.org/data/2.5/weather?zip=20721,us=' + apiKey, function(res) {
+	console.log(res.statusCode);
+});
