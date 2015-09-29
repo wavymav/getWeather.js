c3831572e0354a8cd246f81f864152209a9a378e
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-28 21:39:38 -0400

    handled the status code errors on the response object

--- weather.js
+++ weather.js
@@ -39,8 +39,9 @@ var errorConsoleMessage = function(error) {
 // =======================================================================
 
 // Setting up the http .get() method
-var req = http.get('http://.openweathermap.org/data/2.5/weather?zip=20740,us&units=imperial=' + apiKey, function(res) {
+var req = http.get('http://api.openweathermap.org/data/2.5/weather1212?zip=20740,us&units=imperial=' + apiKey, function(res) {
 	console.log(res.statusCode);
+	console.log(http.STATUS_CODES[res.statusCode]);
 
 	// =====================================================================
 
@@ -58,22 +59,29 @@ var req = http.get('http://.openweathermap.org/data/2.5/weather?zip=20740,us&uni
 	// Using the 'end' event handler to access the data stored in bodyData after the
 	// concatenation of data complets in the 'data' event handler.
 	res.on('end', function() {
-		// Using the try catch block to find any parsing errors
-		try {
-			// Parsing the JSON data & storing it in weatherData var
-			var weatherData = JSON.parse(bodyData);
 
-			// storing the weatherData property values for current temperature & location
-			var temperature = weatherData.main.temp,
-					location = weatherData.name;
+		// If the response statusCode is 200 (OK)
+		if (res.statusCode === 200) {
+			// Using the try catch block to find any parsing errors
+			try {
+				// Parsing the JSON data & storing it in weatherData var
+				var weatherData = JSON.parse(bodyData);
 
-			// Invoking the consoleMessage() with the property values of weatherData
-			consoleMessage(location, temperature);
+				// storing the weatherData property values for current temperature & location
+				var temperature = weatherData.main.temp,
+						location = weatherData.name;
 
-		} catch (err) {
-			// Cating the parse error
-			// Invoking the errorConsoleMessage()
-			errorConsoleMessage(err);
+				// Invoking the consoleMessage() with the property values of weatherData
+				consoleMessage(location, temperature);
+
+			} catch (err) {
+				// Cating the parse error
+				// Invoking the errorConsoleMessage()
+				errorConsoleMessage(err);
+			}
+		} else {
+			// logging out the response status code description if it's not 200 (OK)
+			errorConsoleMessage({message: 'There was an error with the reponse! ('+ http.STATUS_CODES[res.statusCode] +')'});
 		}
 	});
 });
